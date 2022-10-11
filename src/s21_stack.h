4ad039b7151ca3acb225a41cbcce7315b0e8692f
Dima <dimas12343242@gmail.com>
2022-10-11 11:08:01 +0300

    dima start working

---
+++ src/s21_stack.h
@@ -0,0 +1,4 @@
+#ifndef _SRC_S21_STACK_H_
+#define _SRC_S21_STACK_H_
+
+#endif // _SRC_S21_STACK_H_
---
+++ src/s21_vector.h
@@ -0,0 +1,18 @@
+#ifndef _SRC_S21_VECTOR_H_
+#define _SRC_S21_VECTOR_H_
+
+#include <cstddef>
+#include <cstring>
+#include <iostream>
+#include <utility>
+
+namespace s21 {
+
+template <
+    class T,
+    class Allocator = std::allocator<T>
+> class vector;
+    
+} // namespace s21
+
+#endif // _SRC_S21_VECTOR_H_
