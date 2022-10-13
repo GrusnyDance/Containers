336362434defb8eae39c4d802631a5d710683904
GrusnyDance <grusny.dance@gmail.com>
2022-10-13 12:16:46 +0200

    working on TreeNode class

---
+++ src/s21_tree.h
@@ -0,0 +1,42 @@
+#include <iostream>
+
+template <typename Data>
+class TreeNode {
+ public:
+  Data data_;
+  TreeNode* left_;
+  TreeNode* right_;
+  TreeNode* parent_;
+
+  // конструктор без параметров
+  TreeNode() { left_ = right_ = parent_ = nullptr; }
+  // конструктор с параметрами
+  explicit TreeNode(Data data, TreeNode* parent = NULL, TreeNode* left = NULL,
+                    TreeNode* right = NULL) {
+    data_ = data;
+    parent_ = parent;
+    left_ = left;
+    right_ = right;
+  }
+
+  // конструктор копирования
+  TreeNode(const TreeNode& other) { *this = other; }
+  // конструктор перемещения, нет динамической памяти
+  TreeNode(TreeNode&& other) = default;
+  // оператор присваивания копированием
+  TreeNode& operator=(const TreeNode& other) {
+    if (this != &other) {
+      data_ = other.data_;
+      parent_ = other.parent_;
+      left_ = other.left_;
+      right_ = other.right_;
+    }
+    return *this;
+  }
+  ~TreeNode() {}
+
+  // функции для поиска предыдущего и следующего узла? разобраться с итератором
+};
+
+template <typename Т>
+class S21Tree {};
