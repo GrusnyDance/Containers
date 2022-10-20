375322ddc4dec1516c983f95a3105fa2d6e9a16c
Dima <dimas12343242@gmail.com>
2022-10-20 18:27:41 +0300

    vector + tests

--- src/test_for_vector.cc
+++ src/test_for_vector.cc
@@ -5,40 +5,38 @@
 #define CT std::cout << "            " <<
 #define EN std::endl;
 
-template <class T>
-using vector = s21::vector<T>;
+template <class T> using vector = s21::vector<T>;
 
-template <class T>
-using original_vector = std::vector<T>;
+template <class T> using original_vector = std::vector<T>;
 
 class S21VectorTest : public ::testing::Test {};
 
-// TEST(S21VectorTest, Empty) {
-//   vector<int> A;
-//   EXPECT_EQ(A.empty(), true);
-//   A.push_back(1);
-//   EXPECT_EQ(A.empty(), false);
-//   const vector<int> B;
-//   EXPECT_EQ(B.empty(), true);
-// }
+TEST(S21VectorTest, Empty) {
+  vector<int> A;
+  EXPECT_EQ(A.empty(), true);
+  A.push_back(1);
+  EXPECT_EQ(A.empty(), false);
+  const vector<int> B;
+  EXPECT_EQ(B.empty(), true);
+}
 
-// TEST(S21VectorTest, Size) {
-//   vector<int> A;
-//   EXPECT_EQ(A.size(), 0);
-//   A.push_back(1);
-//   EXPECT_EQ(A.size(), 1);
-//   const vector<int> B;
-//   EXPECT_EQ(B.size(), 0);
-// }
+TEST(S21VectorTest, Size) {
+  vector<int> A;
+  EXPECT_EQ(A.size(), 0);
+  A.push_back(1);
+  EXPECT_EQ(A.size(), 1);
+  const vector<int> B;
+  EXPECT_EQ(B.size(), 0);
+}
 
-// TEST(S21VectorTest, FrontBack) {
-//   vector<int> A({1, 2, 3, 4, 5});
-//   const vector<int> B({1, 2, 3, 4, 5});
-//   EXPECT_EQ(A.front(), 1);
-// //   EXPECT_EQ(B.front(), 1);
-//   EXPECT_EQ(A.back(), 5);
-// //   EXPECT_EQ(B.back(), 5);
-// }
+TEST(S21VectorTest, FrontBack) {
+  vector<int> A({1, 2, 3, 4, 5});
+  const vector<int> B({1, 2, 3, 4, 5});
+  EXPECT_EQ(A.front(), 1);
+  EXPECT_EQ(B.front(), 1);
+  EXPECT_EQ(A.back(), 5);
+  EXPECT_EQ(B.back(), 5);
+}
 
 TEST(S21VectorTest, DefaultConstructor) {
   vector<int> A;
@@ -278,80 +276,75 @@ TEST(S21VectorTest, Emplace) {
   }
 }
 
-
-
 TEST(vector, constructor) {
-    vector<int>a{555, 1, 2, 3, 4};
-    vector<int>b(a);
+  vector<int> a{555, 1, 2, 3, 4};
+  vector<int> b(a);
 
-    vector<int>c(b);
+  vector<int> c(b);
 
-    ASSERT_EQ(a[0], c[0]);
+  ASSERT_EQ(a[0], c[0]);
 }
 
 TEST(vector, at) {
-    vector<int>a{1, 2, 3, 4};
-    CT "a.at(3) = " << a.at(3) << EN
-    a.at(3) = 5;
-    ASSERT_EQ(5, a.at(3));
+  vector<int> a{1, 2, 3, 4};
+  CT "a.at(3) = " << a.at(3) << EN a.at(3) = 5;
+  ASSERT_EQ(5, a.at(3));
 }
 
 TEST(vector, FrontBack) {
-    vector<int>a{1, 2, 3, -555};
-    ASSERT_EQ(a.front(), 1);
-    ASSERT_EQ(a.back(), -555);
+  vector<int> a{1, 2, 3, -555};
+  ASSERT_EQ(a.front(), 1);
+  ASSERT_EQ(a.back(), -555);
 }
 
 TEST(vector, data) {
-    vector<int>a{213, 4, 432, 42, 4, 2341};
-    CT a.data();
+  vector<int> a{213, 4, 432, 42, 4, 2341};
+  CT a.data();
 }
 
 TEST(vector, reserve_shrink_to_fit) {
-    vector<int>a{1, 2, 3, 4, 5, 6};
-    a.reserve(10);
-    a[10] = 10;
-    CT "THAN = " << a[10] << EN
-    ASSERT_EQ(a[10], 10);
-    a.shrink_to_fit();
-    CT "NOW  = " << a[10] << EN
+  vector<int> a{1, 2, 3, 4, 5, 6};
+  a.reserve(10);
+  a[10] = 10;
+  CT "THAN = " << a[10] << EN ASSERT_EQ(a[10], 10);
+  a.shrink_to_fit();
+  CT "NOW  = " << a[10] << EN
 }
 
 TEST(vector, clear) {
-    vector<int>a{1, 2, 3, 4, 5, 6};
-    vector<int>b{-1, -2, -3, -4, -5, -6};
-    for (int i = 0; i < 6; i++) {
-    }
-    CT EN
-    a.swap(b);
-    for (int i = 0; i < 6; i++) {
-        ASSERT_TRUE(a[i] < 0);
-    }
-    a.clear();
-    b.clear();
+  vector<int> a{1, 2, 3, 4, 5, 6};
+  vector<int> b{-1, -2, -3, -4, -5, -6};
+  for (int i = 0; i < 6; i++) {
+  }
+  CT EN a.swap(b);
+  for (int i = 0; i < 6; i++) {
+    ASSERT_TRUE(a[i] < 0);
+  }
+  a.clear();
+  b.clear();
 }
 
 TEST(vector, PushPopInsert) {
-    vector<int>a{1,2,3,4,5};
+  vector<int> a{1, 2, 3, 4, 5};
 
-    a.push_back(6);
+  a.push_back(6);
 
-    ASSERT_EQ(a[5], 6);
+  ASSERT_EQ(a[5], 6);
 
-    a.pop_back();
+  a.pop_back();
 
-    ASSERT_EQ(a.size(), 5);
+  ASSERT_EQ(a.size(), 5);
 }
 
 TEST(vector, eraece) {
-    vector<int>a{1,2,3,4,5};
-    a.erase(a.begin());
-    ASSERT_EQ(a[0], 2);
-    a.pop_back();
-    ASSERT_EQ(a.size(), 3);
+  vector<int> a{1, 2, 3, 4, 5};
+  a.erase(a.begin());
+  ASSERT_EQ(a[0], 2);
+  a.pop_back();
+  ASSERT_EQ(a.size(), 3);
 }
 
 int main() {
-    testing::InitGoogleTest();
-    return RUN_ALL_TESTS();
+  testing::InitGoogleTest();
+  return RUN_ALL_TESTS();
 }
