e1f4ec4444df003edababced594059583851a215
Dima <dimas12343242@gmail.com>
2022-10-25 19:36:43 +0300

    stack and tests done

--- src/s21_stack.h
+++ src/s21_stack.h
@@ -7,96 +7,112 @@
 #include <utility>
 
 namespace s21 {
-template <class T>
-struct node {
-    node *next;
-    T value;
+template <class T> struct node {
+  node *next;
+  T value;
 };
 
-template <class T>
-class stack {
-    using value_type = T;
-    using reference = T&;
-    using const_reference = const T&;
-    using size_type = size_t;
-    using Node = node<T>;
+template <class T> class stack {
+  using value_type = T;
+  using reference = T &;
+  using const_reference = const T &;
+  using size_type = size_t;
+  using Node = node<T>;
 
-    public:
-    // constructors
-    stack() : head_(nullptr), size_(0) {}
+public:
+  // constructors
+  stack() : head_(nullptr), size_(0) {}
 
-    stack(std::initializer_list<value_type> const& items): head_(nullptr), size_(0) {
-        for (auto i : items) {
-            this->push(i);
-        }
+  stack(std::initializer_list<value_type> const &items)
+      : head_(nullptr), size_(0) {
+    for (auto i : items) {
+      this->push(i);
+    }
+  }
+
+  stack(const stack &s) : head_(nullptr), size_(0) {
+    stack<value_type> help;
+    Node *n = s.head_;
+    while (n != nullptr) {
+      help.push(n->value);
+      n = n->next;
     }
 
-    stack(const stack &s) : head_(nullptr), size_(0) {
-        stack<value_type> help;
-        Node* n = s.head_;
-        while (n != nullptr) {
-            help.push(n->value);
-            n = n->next;
-        }
+    while (help.size_ != 0) {
+      this->push(help.top());
+      help.pop();
+    }
+  }
 
-        while (help.size_ != 0) {
-            this->push(help.top());
-            help.pop();
-        }
+  stack(stack &&s) {
+    head_ = s.head_;
+    size_ = s.size_;
+    s.head_ = nullptr;
+    s.size_ = 0;
+  }
+
+  ~stack() {
+    while (size_ != 0) {
+      this->pop();
+    }
+  }
+
+  stack &operator=(stack &s) {
+    stack<value_type> help;
+    Node *n = s.head_;
+    while (n != nullptr) {
+      help.push(n->value);
+      n = n->next;
     }
 
-    stack(stack &&s) {
-        this = s;
+    while (help.size_ != 0) {
+      this->push(help.top());
+      help.pop();
+    }
+  }
+
+  stack &operator=(stack &&s) {
+    if (s == nullptr)
+      return nullptr;
+
+    head_ = s.head_;
+    size_ = s.size_;
+    s.head_ = nullptr;
+    s.size_ = 0;
+  }
+
+  // Methods
+
+  const_reference top() { return head_->value; }
+
+  void push(const_reference value) {
+    size_++;
+    Node *tmp = new Node();
+    tmp->value = value;
+    tmp->next = head_;
+    head_ = tmp;
+  }
+
+  void pop() {
+    if (size_ == 0 || head_ == nullptr) {
+      return;
     }
 
-    ~stack() {
-        while (size_ != 0) {
-            std::cout << "DISRTUCT " << "value = " << this->head_->value << "\n";
-            this->pop();
-        }
-    }
+    size_--;
+    Node *out;
+    out = head_;
+    head_ = head_->next;
+    delete out;
+  }
 
-    stack& operator=(stack &&s) {
-        if (s == nullptr)
-            return nullptr;
+  void swap(stack &s) {
+    std::swap(this->head_, s.head_);
+    std::swap(this->size_, s.size_);
+  }
 
-        head_ = s.head_;
-        size_ = s.size_;
-        s.head_ = nullptr;
-        s.size_ = 0;
-    }
-
-    // Methods
-
-    const_reference top() {
-        return head_->value;
-    }
-
-    void push(const_reference value) {
-        size_++;
-        Node* tmp = new Node();
-        tmp->value = value;
-        tmp->next = head_;
-        head_ = tmp;
-    }
-
-    void pop() {
-        if (size_ == 0 || head_ == nullptr){
-            return;
-        }
-
-        size_--;
-        Node *out;
-        out = head_;
-        head_ = head_->next;
-        delete out;
-    }
-
-    void swap();
-
-    private:
-    Node *head_;
-    size_type size_;
+private:
+  Node *head_;
+  size_type size_;
 };
-}
+} // namespace s21
 #endif // _SRC_S21_STACK_H_
--- src/test_for_stack.cc
+++ src/test_for_stack.cc
@@ -5,11 +5,9 @@
 #define CT std::cout << "            " <<
 #define EN std::endl;
 
-template <class T>
-using stack = s21::stack<T>;
+template <class T> using stack = s21::stack<T>;
 
-template <class T>
-using original_stack = std::stack<T>;
+template <class T> using original_stack = std::stack<T>;
 
 TEST(stack, constructors) {
   stack<int> a;
@@ -35,15 +33,78 @@ TEST(stack, copy) {
 
   for (int i = 4; i > 0; i--) {
     ASSERT_EQ(a.top(), i);
-    CT "b = " << b.top() << "   a = " << a.top() << EN
     a.pop();
     b.pop();
+  }
+
+  stack<int> lol{1, 2, 3, 4, 5};
+  stack<int> kek = std::move(lol);
+}
+
+TEST(S21StackTest, DefaultConstructor) {
+  stack<int> A;
+  const stack<int> AA;
+  original_stack<int> B;
+  const original_stack<int> BB;
+}
+
+TEST(S21StackTest, Constructors) {
+  stack<int> A;
+  original_stack<int> B;
+  for (int i = 0; i < 100; i++) {
+    A.push(i);
+    B.push(i);
+  }
+  stack<int> A_copy(A);
+  stack<int> A_move(std::move(A));
+  for (int i = 0; i < 100; i++) {
+    EXPECT_EQ(A_copy.top(), A_move.top());
+    A_copy.pop();
+    A_move.pop();
+  }
+}
+
+TEST(S21StackTest, InitializerConstructor) {
+  stack<int> A({1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+  original_stack<int> B({1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+  while (B.size() != 0) {
+    EXPECT_EQ(A.top(), B.top());
+    A.pop();
+    B.pop();
+  }
+}
+
+TEST(S21StackTest, PushAndPop) {
+  stack<int> A;
+  original_stack<int> B;
+  for (int i = 0; i < 100; i++) {
+    A.push(i);
+    B.push(i);
+  }
+  for (int i = 0; i < 100; i++) {
+    EXPECT_EQ(A.top(), B.top());
+    A.pop();
+    B.pop();
+  }
+}
+
+TEST(S21StackTest, PushPop2) {
+  stack<int> A;
+  original_stack<int> B;
+  for (int i = 0; i < 100; i++) {
+    A.push(i);
+    B.push(i);
+  }
+  for (int i = 0; i < 100; i++) {
+    EXPECT_EQ(A.top(), B.top());
+    A.pop();
+    B.pop();
   }
 }
 
 TEST(stack, PushPop) {
   stack<int> a;
-  original_stack<int>b;
+  original_stack<int> b;
   for (int i = 0; i < 1000; i++) {
     a.push(i);
     b.push(i);
